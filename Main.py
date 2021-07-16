cbf80b4919febad1b8786317a1d9caf28ee99f4c
Richard Thio <richardthiorichard@gmail.com>
2021-07-16 10:55:42 +0100

    Updated and fixed bugs regarding input layer dynamic size

--- Main.py
+++ Main.py
@@ -7,12 +7,13 @@ from Data_Proc import Data_Set_Proc
 get the processed data
 '''
 data_set_proc = Data_Set_Proc()
-data_set = data_set_proc.get_data_set('C:/Users/richa/Desktop/Crypto_Cheat/CryptoCheat/raw_historic_data/2021_01__BTC-USD.json')
+data_set = data_set_proc.get_data_set('C:/Users/richa/Desktop/Crypto_Cheat/CryptoCheat/raw_historic_data/2020_01__BTC-USD.json')
 data_set = np.append(data_set, [-1],axis = 0)
+data_set_size = len(data_set)
 
 '''
 generate neural network and feed forward to get output
 '''
-neural_model = Neural_Model([186,10,10,1], -1)
+neural_model = Neural_Model([data_set_size-1,10,10,1], -1)
 a = neural_model.forward(data_set)
 print("Neural network output :", a)
