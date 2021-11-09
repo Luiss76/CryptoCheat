4d7a931c1d13258822df2755694118077e4c2358
Richard Thio <richardthiorichard@gmail.com>
2021-11-09 14:54:55 +0000

    Update: Bitfinex API
    -Made a function to incorporate bitfinex API

--- Import_Data.py
+++ Import_Data.py
@@ -3,13 +3,14 @@ import json
 from numpyencoder import NumpyEncoder
 
 
-s_year = 2021
+s_year = 2017
 s_month = 1
-n_year = 2021
-n_month = 2
+n_year = 2017
+n_month = 12
+timeframe = '1h'
 data_set_proc = Data_Set_Proc()
-numpy_data = data_set_proc.get_data_set_bitfinex(2021,2021,1,2)  #start year, end year, start month, end month
-with open('raw_historic_data/'+ str(s_year) + '_' +str(s_month) +'_to_'+ str(n_year) + '_' +str(n_month) +'_data.json', 'w') as file:
+numpy_data = data_set_proc.get_data(s_year,n_year,s_month,n_month,timeframe)  #start year, end year, start month, end month
+with open('historical_data/'+ timeframe+'_'+str(s_year) + '_' +str(s_month) +'_to_'+ str(n_year) + '_' +str(n_month) +'_data.json', 'w') as file:
     json.dump(numpy_data, file, indent=4, sort_keys=True,
               separators=(', ', ': '), ensure_ascii=False,
               cls=NumpyEncoder)
