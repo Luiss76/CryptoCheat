d7e3d5273778bddabeeb196a08db43dba586ec08
Richard Thio <richardthiorichard@gmail.com>
2021-11-09 14:18:02 +0000

    Feature: Import model, error or evaluation, tanh activation function
    -import model feature to allow importing past neural network weights
    
    -error function feature to calculate the error or evaluation neural network for prediction price
    
    -tanh function to sync with the normalisation range from -1 to 1

--- Neural_Model.py
+++ Neural_Model.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import json
 import matplotlib.pyplot as plt
+import statistics as st
 
 class Neural_Model:
 	'''
@@ -21,6 +22,7 @@ class Neural_Model:
 		self.n_layer = len(n_neuron) - 1
 		self.network_topology = np.empty(self.n_layer, dtype= object)
 		self.score = 0
+		self.valid_score = 0
 
 		'''
 		Iterate through the neural topology and generate random neuron weight 
@@ -35,34 +37,157 @@ class Neural_Model:
 	feed forward inputs to the network to get output
 
 	'''
+	def import_model(self,neural_model,address):
+		neural_weights = np.load(address,allow_pickle = True)
+		shape_arr =[len(neural_weights[0])]
+		for i in range(1,len(neural_weights)):
+			shape_arr.append(len(neural_weights[i])-1)
+
+		neural_model = Neural_Model([shape_arr],-1)
+		neural_model.network_topology = neural_weights
+
 	def forward(self, inputs):
+	
+		inputs = inputs.flatten()
+		inputs = np.reshape(inputs,(1,len(inputs)))
+		#print("shape of input :",np.shape(inputs))
 		max_range = np.size(self.network_topology)
 		for i in range(max_range):
 			if(i < 1):
 
-				output = np.dot(inputs,self.network_topology[0]) 
+				output = np.dot(inputs,self.network_topology[0])
 				output = self.sigmoid_funct(output)
-				output = np.append(output,[-1],axis = 0)
-				#output = np.concatenate((output,-np.ones((np.shape(output)[0],1))),axis=1) 
+				#print("output before appended :", output)
+				output = np.concatenate((output,-np.ones((np.shape(output)[0],1))),axis=1)
+				#output = np.concatenate((output,-np.ones((np.shape(output)[0],1))),axis=1)
 				
 
 			elif(i < max_range - 1):
 
 				output = np.dot(output,self.network_topology[i])
 				output = self.sigmoid_funct(output)
-				output = np.append(output,[-1],axis = 0)
-
+				output = np.concatenate((output,-np.ones((np.shape(output)[0],1))),axis=1)
 			else:
 				output = np.dot(output,self.network_topology[i]) 
-				output = self.sigmoid_funct(output)
+				output = self.tanh(output)
 				return output
+
+	def error_funct(self,output,target):
+		#print("targets :",np.shape(target))
+		#print("targets :", target)
+		#print("max value :",max(target))		
+
+		upper_error = abs(max(target) - output)
+		lower_error = abs(min(target) - output)
+
+		mean_target = st.mean(target)
+		median_target = st.median(target)
+		diff = 0
+		weight = 1.0
+		w_arr = []
+
+		time_weight_interval = 0.2/len(target)
+		time_weight = time_weight_interval
+		tw_arr = []
+
+
+		for i in range(len(target)):
+			w_arr = np.append(w_arr,abs((target[i] - median_target)))
+			tw_arr = np.append(tw_arr,time_weight)
+			time_weight = time_weight + time_weight_interval
+			
+		w_arr = w_arr/max(w_arr)
+		#print("time weight :",tw_arr)
+		
+		
+
+		w_arr = (abs((w_arr - st.mean(w_arr))/st.stdev(w_arr))) + 1
+		
+		w_arr = (w_arr / (1.5*max(w_arr)))+1
+		#w_arr [np.argmax(w_arr)] = w_arr [np.argmax(w_arr)] * 0.5
+		#print("w_arr :",w_arr)
+		w_arr = w_arr + tw_arr
+		#print("newly added w_arr:",w_arr)
+		
+		for i in range(len(target)):
+			#print("diff added by :", (target[i] - mean_target)*w_arr[i])
+			diff = diff + ((target[i] - mean_target)*w_arr[i])
+			#print("diff :", diff)
+			#diff = diff + ((target[i] - mean_target)*weight)
+			#weight = weight + 0.05
+
+		diff = diff + mean_target
+		error = abs(output - diff)
+		#if(upper_error > lower_error):
+		#	error = lower_error
+		#else:
+		#		error = upper_error
+		#print("diff:",diff)
+		output = output.reshape(1,)
+		#x = [0,12]
+		#y = [output,output]
+		#c = [median_target, median_target]
+		#p = [mean_target, mean_target]
+		#l = [diff , diff]
+		#print("shape of output :", np.shape(output))
+		#plt.plot(target,'r')
+		#plt.plot(x,p,'g')
+		#plt.plot(x,c,'b')
+		#plt.plot(x,l,'y')
+		#plt.show()
+
+		return error
+
+	def get_diff(self,target):
+		mean_target = st.mean(target)
+		median_target = st.median(target)
+		diff = 0
+		weight = 1.0
+		w_arr = []
+
+		time_weight_interval = 0.2/len(target)
+		time_weight = time_weight_interval
+		tw_arr = []
+
+
+		for i in range(len(target)):
+			w_arr = np.append(w_arr,abs((target[i] - median_target)))
+			tw_arr = np.append(tw_arr,time_weight)
+			time_weight = time_weight + time_weight_interval
+			
+		w_arr = w_arr/max(w_arr)
+		#print("time weight :",tw_arr)
+		
+		
+
+		w_arr = (abs((w_arr - st.mean(w_arr))/st.stdev(w_arr))) + 1
+		
+		w_arr = (w_arr / (1.5*max(w_arr)))+1
+		#w_arr [np.argmax(w_arr)] = w_arr [np.argmax(w_arr)] * 0.5
+		#print("w_arr :",w_arr)
+		w_arr = w_arr + tw_arr
+		#print("newly added w_arr:",w_arr)
+		
+		for i in range(len(target)):
+			#print("diff added by :", (target[i] - mean_target)*w_arr[i])
+			diff = diff + ((target[i] - mean_target)*w_arr[i])
+			#print("diff :", diff)
+			#diff = diff + ((target[i] - mean_target)*weight)
+			#weight = weight + 0.05
+
+		diff = diff + mean_target
+
+		return diff
 
 		
 	'''
 	sigmoid activation function
 	'''
 	def sigmoid_funct(self,inputs):
-
+		inputs = np.clip(inputs, -501,501)
 		return 1.0/(1.0+np.exp(self.beta*inputs))
 
-
+	def tanh(self,inputs):
+		inputs = np.clip(inputs,-501,501)
+		y = (np.exp(inputs) - np.exp(-inputs))/(np.exp(inputs) + np.exp(-inputs))
+		return y
