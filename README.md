460ae4d7b6d7eaa0627b01a21dbd6d4f87e744c8
Richard Thio <richardthiorichard@gmail.com>
2021-11-09 14:55:41 +0000

    Deleted Files

--- README.md
+++
@@ -1,2 +0,0 @@
-# CryptoCheat
-To predict buyor sell cryptocurrency
--- mynumpy.py
+++
@@ -1,5 +0,0 @@
-from numpy import *
-_empty = empty
-def empty(*args, **kwargs):
-    kwargs.update(dtype=float128)
-    _empty(*args, **kwargs)
